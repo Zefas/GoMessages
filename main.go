eb36f5eefd590a89eeb1354eab50296d441467f2
Zefas <vaidas.1s@gmail.com>
2015-11-04 14:33:13 +0200

    Project init - basic http server.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/send", handleSend)
+	http.HandleFunc("/receive", handleReceive)
+
+	err := http.ListenAndServe(":9090", nil);
+	if err != nil {
+		fmt.Println("Error while starting server!")
+	}
 }
+
+
+
 
